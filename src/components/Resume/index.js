3f757d65c6d0e7edcd744ffc22ac72efae329b13
Nadya <hoperu1986@gmail.com>
2022-08-16 12:30:17 -0400

    resume component added

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 function Resume() {
     return (
-<div>Resume</div>
+<div>
+    <h1>Resume</h1>
+    <h3>Download my <a href={require(`./Resume.docx`)}>resume</a></h3>
+    <ul>Front-end Proficiencies:
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>responsive design</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+    </ul>
+    <ul>Back-end Proficiencies:
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL, Sequelize</li>
+        <li>MongoDB, Mongoose</li>
+        <li>REST</li>
+        <li>GraphQL</li>
+    </ul>
+</div>
     );
   }
   
